9ec0522ffab4b2d3fb210d771d93156f3adb36b9
ontu001 <mdrohanurrahmanontu@gmail.com>
2025-05-15 20:42:29 +0600

    Additional analysis done

--- maven_coffee_sales_analysis.sql
+++ maven_coffee_sales_analysis.sql
@@ -14,9 +14,11 @@ CREATE TABLE transactions (
     product_detail VARCHAR(100)
 );
 
+-- EDA Maven Coffee sales --
 
 
--- Undderstand the data
+
+-- Undderstand the data -- 
 select *
 from transactions;
 
@@ -29,12 +31,54 @@ WHERE table_name = 'transactions';
 
 
 
--- EDA Maven Coffee sales --
+
+
+
+-- Total Revenue
+select sum(unit_price * transaction_qty) as Total_Revenue
+from transactions;
+
+
+-- Avegrage Order Value AOV
+select
+	round(sum(transaction_qty * unit_price) / count(distinct transaction_id), 2) as AOV
+from transactions;
+
+
+
+
+-- 🏪 Store Performance --
+
+-- Which store location has the highest total sales?
+select store_location, sum(unit_price * transaction_qty) as total_sales
+from transactions
+group by 1
+order by 2 desc;
+
+
+
+-- Which store has the highest average transaction value?
+select store_location,
+	round(avg(unit_price * transaction_qty),  2) as total_sales
+from transactions
+group by 1
+order by 2 desc;
+
+
+
+-- 📅 Time-Based Trends --
+
+-- What is the hourly sales trend? (peak sales hours)?
+select extract(hour from transaction_time) as hour_ , sum(transaction_qty) as total_sales
+from transactions
+group by 1
+order by 2 desc;
 
 -- Total Transaction of APRIL 2023
 select count(transaction_id)
 from transactions
 where transaction_date between '2023-04-01' and '2023-04-30';
+
 
 -- MOM Transaction Trend
 select extract(month from transaction_date) as Month_number, count(transaction_id) as total_transaction
@@ -42,10 +86,16 @@ from transactions
 group by 1;
 
 
--- Total Revenue
-select sum(unit_price * transaction_qty) as Total_Revenue
-from transactions;
+-- Day of Week analysis: Which day sells the most?
+select DAYNAME(transaction_date) AS day_of_week, 
+		sum(transaction_qty) as total_sales
+from transactions
+group by 1
+order by 2 desc;
 
+
+
+-- 📦 Product Insights --
 
 
 -- Best Selling Product Based on daily average sell
@@ -59,10 +109,23 @@ group by 1
 order by 4 desc;
 
 
--- Avegrage Order Value AOV
+
+-- Find all product type under every product category
 select
-	round(sum(transaction_qty * unit_price) / count(distinct transaction_id), 2) as AOV
-from transactions;
+	product_category,
+    group_concat(distinct product_type separator ',') as Products
+from transactions
+group by 1;
+
+
+
+
+-- Which product category generates the highest revenue?
+select product_category,
+		sum(unit_price * transaction_qty) as revenue
+from transactions
+group by 1
+order by 2 desc;
 
 
 
@@ -82,14 +145,18 @@ order by 2 desc;
 
 
 
--- LIKE , ILIKE, IN, BETWEEN , ISNULL
-select product_category, product_detail
+-- Which specific product has the highest revenue per unit sold?
+select product_detail,
+		round(sum(unit_price * transaction_qty) / sum(transaction_qty), 2) as  revenue_per_unit
 from transactions
-where product_detail like 'G%';
+group by 1
+order by 2 desc;
 
--- Find all product type under every product category
-select
-	product_category,
-    group_concat(distinct product_type separator ',') as Products
-from transactions
-group by 1;
+
+
+-- 👥 Customer Behavior (Inferred)👥 Customer Behavior (Inferred) --
+
+-- What is the average number of items per transaction?
+select round(sum(transaction_qty) / count(distinct transaction_id), 2) as avg_items_per_transaction
+from transactions;
+
